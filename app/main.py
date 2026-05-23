d2e2c1482769584f704a891791eb85ebdc0b30bc
Your Name <you@example.com>
2026-05-23 12:59:15 +0530

    fix cors: allow_credentials must be false when allow_origins is wildcard

--- app/main.py
+++ app/main.py
@@ -26,7 +26,7 @@ _allowed = os.environ.get("FRONTEND_URL", "*")
 app.add_middleware(
     CORSMiddleware,
     allow_origins=[_allowed] if _allowed != "*" else ["*"],
-    allow_credentials=True,
+    allow_credentials=False,
     allow_methods=["*"],
     allow_headers=["*"],
 )
